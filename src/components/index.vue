70cae5cc4b6086a120099fdd0ec5f59aa26b927e
JkYou <ups2017@163.com>
2017-09-26 22:34:09 +0800

    first commit

---
+++ src/components/index.vue
@@ -0,0 +1,20 @@
+<template>
+
+</template>
+
+<script type="text/ecmascript-6">
+    export default {
+        data() {
+            return {};
+
+        },
+        created() {
+
+        },
+
+    }
+</script>
+
+<style lang="less" scoped>
+
+</style>
