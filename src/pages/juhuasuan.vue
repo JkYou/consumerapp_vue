8583ab56916de637d6b6e980bf69b81cb1cf0a8e
黄克琛 <huangkechen@huangkehendeMBP.lan>
2018-03-01 12:35:24 +0800

    老游傻逼

--- src/pages/juhuasuan.vue
+++ src/pages/juhuasuan.vue
@@ -1,13 +1,11 @@
 <template>
-	<div id="root">
-		<p class="emp1">Jk祝您新年快乐,万事如意！</p>
-		<p class="emp">程序员哥哥正在熬夜加班开发中...</p>
-	<tabbar></tabbar></div>
-	
+<div>
+  <banner></banner>
+</div>
 </template>
 
 <script>
-import tabbar from '@/components/Tabbar'
+import banner from '@/components/banner'
 export default {
 
   name: 'juhuasuan',
@@ -18,39 +16,11 @@ export default {
     }
   },
    components:{
-       tabbar,
+     banner,
         },
 }
 </script>
 
 <style lang="less" scoped>
-#root{
-		width: 100%;
-		height: 100%;
-		background-image: url(../assets/happy.jpg);
-		background-size:cover;
-		background-repeat: no-repeat;
-		position: absolute;
-		top: 0px;
-		bottom: 0px;
-		.emp1{
-			width: 100%;
-			position: absolute;
-		    color: #ffffff;
-		    text-align: center;
-		    top: 200px;
-		    font-size: 30px;
-		    text-shadow: 1px 1px 5px #ffffff;
-
-		}
-	.emp{
-		width: 100%;
-		position: absolute;
-	    color: #ffffff;
-	    font-size: 15px;
-	    text-align: center;
-	    bottom: 180px;
-}
-}
 
 </style>
