7fa02488ea136c703df130f535a6a43ecffb7be4
JkYou <ups2017@163.com>
2018-03-21 16:06:23 +0800

    add px2rem

--- build/vue-loader.conf.js
+++ build/vue-loader.conf.js
@@ -3,6 +3,8 @@ var config = require('../config')
 var isProduction = process.env.NODE_ENV === 'production'
 
 module.exports = {
+  //使用px-rem转换
+  postcss: [require('postcss-px2rem')({remUnit: 75})],
   loaders: utils.cssLoaders({
     sourceMap: isProduction
       ? config.build.productionSourceMap
